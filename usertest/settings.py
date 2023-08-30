b5d06a65a8c3906ff3df59e4e8d107b886abab31
progIfe <ifeoluwasamson90@gmail.com>
2023-08-30 23:58:26 +0100

    App serurity

--- usertest/settings.py
+++ usertest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 from django.conf import settings
 from datetime import timedelta
 
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6rzu54zv$6f_1f#v%j2-0xen%us*qh4r(_(+jt^%ck1e(-ym^='
+SECRET_KEY = config('SECRET_KEY', default='mydefaultvalue')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 

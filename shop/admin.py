451316953a729b38d872c170cbd9fadde735aed8
progIfe <ifeoluwasamson90@gmail.com>
2023-09-03 20:56:57 +0100

    Category model migration

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.image.url))
+    
+    thumbnail.short_description = 'photo'
+
+    list_display = ('_id', 'thumbnail', 'name')
+    list_display_links = ('name', 'thumbnail')
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Category, CategoryAdmin)
+    
+    
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,17 @@
+from typing import Any
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/categories/', null=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
